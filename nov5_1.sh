78743b034468fec8d61f6aea09220b3b6aa9ef22
RajGhugare19 <rajghugare.vnit@gmail.com>
2022-11-06 11:25:35 +0530

    adding halfcheetah and ant exps with kl coef

--- nov5_1.sh
+++ nov5_1.sh
@@ -1,5 +1,11 @@
-python train.py id=Humanoid-v2 env_buffer_size=1000000 lambda_cost=0.05 seed=1 wandb_log=True wandb_run_name=alm_lambda0.05_1& 
+python train.py id=HalfCheetah-v2 lambda_cost=0.05 seed=1 num_train_steps=300000 wandb_log=True wandb_run_name=alm_lambda0.05_1& 
 
-python train.py id=Humanoid-v2 env_buffer_size=1000000 lambda_cost=0.05 seed=2 wandb_log=True wandb_run_name=alm_lambda0.05_2& 
+python train.py id=HalfCheetah-v2 lambda_cost=0.05 seed=2 num_train_steps=300000 wandb_log=True wandb_run_name=alm_lambda0.05_2& 
 
-python train.py id=Humanoid-v2 env_buffer_size=1000000 lambda_cost=0.05 seed=3 wandb_log=True wandb_run_name=alm_lambda0.05_3
+python train.py id=HalfCheetah-v2 lambda_cost=0.05 seed=3 num_train_steps=300000 wandb_log=True wandb_run_name=alm_lambda0.05_3&
+
+python train.py id=Ant-v2 lambda_cost=0.05 seed=1 wandb_log=True wandb_run_name=alm_lambda0.05_1& 
+
+python train.py id=Ant-v2 lambda_cost=0.05 seed=2 wandb_log=True wandb_run_name=alm_lambda0.05_2& 
+
+python train.py id=Ant-v2 lambda_cost=0.05 seed=3 wandb_log=True wandb_run_name=alm_lambda0.05_3
