98d177eb6fc12d0022df39ebe9981ee5d1e70953
RajGhugare19 <rajghugare.vnit@gmail.com>
2022-11-07 15:29:46 +0530

    fix run name bug

--- nov5_5.sh
+++ nov5_5.sh
@@ -1,11 +1,11 @@
-python train.py id=Humanoid-v2 identity_encoder=True seed=1 wandb_log=True wandb_run_name=alm_identity_encoder=True_1& 
+python train.py id=Humanoid-v2 identity_encoder=True seed=1 wandb_log=True wandb_run_name=alm_identity_encoder_1& 
 
-python train.py id=Humanoid-v2 identity_encoder=True seed=2 wandb_log=True wandb_run_name=alm_identity_encoder=True_2& 
+python train.py id=Humanoid-v2 identity_encoder=True seed=2 wandb_log=True wandb_run_name=alm_identity_encoder_2& 
 
-python train.py id=Humanoid-v2 identity_encoder=True seed=3 wandb_log=True wandb_run_name=alm_identity_encoder=True_3&
+python train.py id=Humanoid-v2 identity_encoder=True seed=3 wandb_log=True wandb_run_name=alm_identity_encoder_3&
 
-python train.py id=Ant-v2 identity_encoder=True seed=1 wandb_log=True wandb_run_name=alm_identity_encoder=True_1& 
+python train.py id=Ant-v2 identity_encoder=True seed=1 wandb_log=True wandb_run_name=alm_identity_encoder_1& 
 
-python train.py id=Ant-v2 identity_encoder=True seed=2 wandb_log=True wandb_run_name=alm_identity_encoder=True_2& 
+python train.py id=Ant-v2 identity_encoder=True seed=2 wandb_log=True wandb_run_name=alm_identity_encoder_2& 
 
-python train.py id=Ant-v2 identity_encoder=True seed=3 wandb_log=True wandb_run_name=alm_identity_encoder=True_3
+python train.py id=Ant-v2 identity_encoder=True seed=3 wandb_log=True wandb_run_name=alm_identity_encoder_3
