a3e4d79bbda2fcbc8c74e6069edbe081e24a3e71
RajGhugare19 <rajghugare.vnit@gmail.com>
2022-11-07 16:18:31 +0530

    adding coef 0.15, 0.2

---
+++ nov5_10.sh
@@ -0,0 +1,11 @@
+python train.py id=Humanoid-v2 lambda_cost=0.15 seed=1 wandb_log=True wandb_run_name=alm_lambda0.15_1& 
+
+python train.py id=Humanoid-v2 lambda_cost=0.15 seed=2 wandb_log=True wandb_run_name=alm_lambda0.15_2& 
+
+python train.py id=Humanoid-v2 lambda_cost=0.15 seed=3 wandb_log=True wandb_run_name=alm_lambda0.15_3&
+
+python train.py id=Humanoid-v2 lambda_cost=0.2 seed=1 wandb_log=True wandb_run_name=alm_lambda0.2_1& 
+
+python train.py id=Humanoid-v2 lambda_cost=0.2 seed=2 wandb_log=True wandb_run_name=alm_lambda0.2_2& 
+
+python train.py id=Humanoid-v2 lambda_cost=0.2 seed=3 wandb_log=True wandb_run_name=alm_lambda0.2_3&
